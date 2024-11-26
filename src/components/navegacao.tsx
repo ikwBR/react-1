a6d62898b34f76ee00271ce9f0c95d9a4a47e54e
Igor Ichikawa <144551920+ikwBR@users.noreply.github.com>
2024-11-26 18:35:09 -0300

    Create navegacao.tsx

---
+++ src/components/navegacao.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+const Navegacao = () => {
+
+Estrutura do site 2
+
+return (
+<ul>
+<li>
+<a href="#">Home</a>
+</li>
+<li>
+<a href="#">Fronted</a>
+</li>
+<li>
+<a href="#">Programacao</a>
+</li>
+<li>
+<a href="#">Catálogo</a>
+</li>
+</ul>
+);
+};
